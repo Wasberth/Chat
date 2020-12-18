e0524ad815a82b609f4ebf67e3ce482291cea1ee
Wasberth <wdavidgp@hotmail.com>
2020-12-17 22:45:29 -0600

    Chat en NodeJS por Willy

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+var socket = io.connect('http://localhost:25565', { 'forceNew': true });
+
+socket.on('messages', function(data) {
+    console.log(data);
+    render(data);
+});
+
+function render(data) {
+    var html = data.map(elem => `
+                        <div class="d-flex justify-content-start mb-4">
+                            <div class="msg_cotainer">
+                                ${elem.text}
+                                <span class="msg_time">${elem.author}</span>
+                            </div>
+                        </div>`).join("");
+    document.getElementById('messageCount').innerHTML = data.length + " mensajes";
+    document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage() {
+    if (document.getElementById('texto').value != '') {
+        var message = {
+            author: document.getElementById('username').value,
+            text: document.getElementById('texto').value
+        };
+        document.getElementById('texto').value = '';
+        socket.emit('new-message', message);
+    }
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const maxMessages = 40;
+const initialMessage = {
+    text: "Este es mi chat en Node.js. Con fin de optimizar el envío de mensajes, el máximo aceptado es " + maxMessages + " y una vez llegado ese límite los primeros mensajes serán eliminados.",
+    author: "Wilberth Piña"
+};
+
+app.use(express.static('public'));
+
+var messages = [initialMessage];
+
+io.on('connection', function(socket) {
+    console.log('Alguien se ha conectado');
+    socket.emit('messages', messages);
+
+    socket.on('new-message', function(data) {
+        while (messages.length >= maxMessages) {
+            messages.shift();
+        }
+        messages.push(data);
+
+        io.sockets.emit('messages', messages);
+    });
+});
+
+server.listen(25565, function() {
+    console.log("Servidor corriendo en http://localhost:25565");
+});
